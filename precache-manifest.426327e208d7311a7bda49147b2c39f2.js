f7465f3811c6c5b3f745a587554b82edc569694a
dylanherbig <dylanherbig@gmail.com>
2022-09-08 02:57:27 -0400

    transition to hoohacks repository

---
+++ precache-manifest.426327e208d7311a7bda49147b2c39f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c828547fdb5f6c917fcc",
+    "url": "/ideathon-registration/static/js/main.0b651c38.chunk.js"
+  },
+  {
+    "revision": "4a53fa750533176a68ea",
+    "url": "/ideathon-registration/static/js/runtime~main.684056a3.js"
+  },
+  {
+    "revision": "6ca545de4b5d0eaa4dfb",
+    "url": "/ideathon-registration/static/css/2.a885abb4.chunk.css"
+  },
+  {
+    "revision": "6ca545de4b5d0eaa4dfb",
+    "url": "/ideathon-registration/static/js/2.bf5ff9d6.chunk.js"
+  },
+  {
+    "revision": "795b1256150a2f7411e2",
+    "url": "/ideathon-registration/static/js/3.7831f9e5.chunk.js"
+  },
+  {
+    "revision": "8887ae4c8312ae6d2f971c0ab0c2ace4",
+    "url": "/ideathon-registration/static/media/logo.8887ae4c.png"
+  },
+  {
+    "revision": "6c7d9b840c17e38d0e0505623233a38b",
+    "url": "/ideathon-registration/index.html"
+  }
+];
