9586299d852f1428b729347179c14133d5c5c782
Elnora <elnoramagruder@gmail.com>
2023-10-08 19:24:23 -0400

    integrated analytics feature that detches data

--- src/firebase.js
+++ src/firebase.js
@@ -23,6 +23,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const analytics = getAnalytics(app);
 export const storage = getStorage(app);
 export const database = getDatabase(app);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Admin from "./RegisteredAtDisplay";
 import Registration from './Registration';
 import reportWebVitals from './reportWebVitals';
+//import LineGraph from './LineGraph'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Registration route />
-  </React.StrictMode>
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Registration />} />
+        <Route path="/RegisteredAtDisplay" element={<Admin />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
