48c894bdd2058a2f3972d4a6b8804c9dbaae0038
Akhil-prog-design <akhilrdesai@gmail.com>
2023-10-09 22:31:03 +0000

    Created search by dietary restrictions, and put columns

--- src/Search.js
+++ src/Search.js
@@ -8,8 +8,11 @@ import { database } from './firebase';
 function Search() {
     
        const [Query, setQuery] = useState("")
+       const [Resume, setResume] = useState([])
        const [dietaryRestriction, setDietaryRestriction] = useState([])
-       const [filteredData, setFilteredData] = useState([]);
+       const [FullName, setFullName] = useState([]);
+       const [Email, setEmail] = useState([]);
+       const [selectedDietaryRestriction, setSelectedDietaryRestriction] = useState("");
 
        useEffect(() => { 
 
@@ -19,26 +22,36 @@ function Search() {
                 if (data) {
                 const fullNameArray = [];
                 const emailArray = [];
-                const dietaryRestricts = [];
+                const dietaryRestrictsArray = []
+                const resumesArray = [];
 
                 for (const key in data) {
                     if (data.hasOwnProperty(key)) {
                     const entry = data[key];
-                    
-                    const { firstName, lastName, email, dietaryRestriction } = entry;
+                
+                    const { firstName, lastName, email, dietaryRestriction, resume} = entry;
                     const fullName = `${firstName} ${lastName}`;
-                    //TODO figure out why email is not parsing as a string
-                    fullNameArray.push(fullName.toLowerCase()); 
-                    if(email != null){ emailArray.push(email.toString());}
-                    dietaryRestricts.push(dietaryRestriction)
+
+                    if(fullName != "" && fullName != null){fullNameArray.push(fullName.toLowerCase());}
+
+                    if(email != null){emailArray.push(email.toString().toLowerCase());}
+                    
+                    if(dietaryRestriction != null){dietaryRestrictsArray.push(dietaryRestriction.toString().trim().toLowerCase())}
+
+                    if(resume != null){resumesArray.push(resume)
                     }
+                  }
                 }
 
                 const uniqueNames = [...new Set(fullNameArray)];
                 const uniqueEmails = [...new Set(emailArray)];
+                const uniqueResumes = [...new Set(resumesArray)]
             
-                setFilteredData([...uniqueNames, ...uniqueEmails])
-                setDietaryRestriction(dietaryRestricts)
+                setFullName(uniqueNames)
+                setEmail(uniqueEmails)
+                setDietaryRestriction(dietaryRestrictsArray)
+                setResume(uniqueResumes)
+
                 } else {
                 console.log('No data found');
                 }
@@ -46,10 +59,14 @@ function Search() {
         }, []);
 
         
-        const filteredResults = filteredData.filter((item) =>
-              item.toLowerCase().includes(Query.toLowerCase())
-            );
-
+        const filteredResults = FullName.filter((fullName, index) => {
+          const email = Email[index];
+        
+          const matchesQuery = fullName.toLowerCase().includes(Query.toLowerCase()) ||
+            (email && email.toLowerCase().includes(Query.toLowerCase()));
+        
+          return matchesQuery 
+        });
        
 
         return (
@@ -63,14 +80,41 @@ function Search() {
             value={Query}
             onChange={(e) => setQuery(e.target.value)}
           />
+
+          <select
+                value={selectedDietaryRestriction}
+                onChange={(e) => setSelectedDietaryRestriction(e.target.value)}
+              >
+                <option value="">All Dietary Restrictions</option>
+                <option value="vegetarian">Vegetarian</option>
+                <option value="gluten">Gluten-free</option>
+                <option value="other">other</option>
+          </select>
     
-          {/* Display filtered names */}
           <h2>Name and Emails</h2>
-          <ul>
-            {filteredResults.map((results, index) => (
-              <li key={index}>{results} - {dietaryRestriction[index]} </li>
-            ))}
-          </ul>
+          <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", gap: "20px"}}>
+          {filteredResults.map((results, index) => {
+              const dietaryRestrictionValue = dietaryRestriction[index];
+              if (!selectedDietaryRestriction || (dietaryRestrictionValue && dietaryRestrictionValue.includes(selectedDietaryRestriction))) {
+                return (
+                  <div key={index} style={{ border: "1px solid #ccc", padding: "10px" }}>
+                    <p>{results} - {Email.length} - {FullName.length}</p>
+                    <p>{dietaryRestrictionValue}</p>
+                    <p>{Email[index]}</p>
+                    {Resume[index] ? (
+                      <p>
+                        <a href={Resume[index]} target="_blank" rel="noopener noreferrer">
+                          {results} resume
+                        </a>
+                      </p>
+                    ) : null}
+                  </div>
+                );
+              } else {
+                return null; // Do not render if the dietary restriction does not match
+              }
+            })}
+          </div>
         </div>
           );
 
