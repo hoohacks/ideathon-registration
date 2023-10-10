2de815c3ebd4c65bab03d73dc81190636aba7d21
Akhil-prog-design <akhilrdesai@gmail.com>
2023-10-10 17:13:52 +0000

    Refactored person Data

--- src/Search.js
+++ src/Search.js
@@ -8,64 +8,70 @@ import { database } from './firebase';
 function Search() {
     
        const [Query, setQuery] = useState("")
-       const [Resume, setResume] = useState([])
        const [dietaryRestriction, setDietaryRestriction] = useState([])
-       const [FullName, setFullName] = useState([]);
-       const [Email, setEmail] = useState([]);
        const [selectedDietaryRestriction, setSelectedDietaryRestriction] = useState("");
+       // varun stuff
+       const [checkedInStatus, setCheckedInStatus] = useState({}); // Object to store checked-in status
+       const [checkedInCount, setCheckedInCount] = useState(0);
+       
 
-       useEffect(() => { 
 
-            onValue(ref(database), (snapshot) => {
-                const data = snapshot.val();
 
-                if (data) {
-                const fullNameArray = [];
-                const emailArray = [];
-                const dietaryRestrictsArray = []
-                const resumesArray = [];
+       const [Data, setData] = useState({}); 
 
-                for (const key in data) {
-                    if (data.hasOwnProperty(key)) {
-                    const entry = data[key];
-                
-                    const { firstName, lastName, email, dietaryRestriction, resume} = entry;
-                    const fullName = `${firstName} ${lastName}`;
+        useEffect(() => {
+          onValue(ref(database), (snapshot) => {
+            const data = snapshot.val();
 
-                    if(fullName != "" && fullName != null){fullNameArray.push(fullName.toLowerCase());}
+            if (data) {
+              const newData = {}; 
 
-                    if(email != null){emailArray.push(email.toString().toLowerCase());}
-                    
-                    if(dietaryRestriction != null){dietaryRestrictsArray.push(dietaryRestriction.toString().trim().toLowerCase())}
+              for (const key in data) {
+                if (data.hasOwnProperty(key)) {
+                  const entry = data[key];
 
-                    if(resume != null){resumesArray.push(resume)
+                  const { firstName, lastName, email, dietaryRestriction, resume } = entry;
+                  const fullName = `${firstName} ${lastName}`;
+
+                  if (fullName != null) {
+                    if (!newData[fullName]) {
+                      newData[fullName] = [];
+                      newData[fullName].push({ email, dietaryRestriction, resume });
                     }
                   }
                 }
+              }
 
-                const uniqueNames = [...new Set(fullNameArray)];
-                const uniqueEmails = [...new Set(emailArray)];
-                const uniqueResumes = [...new Set(resumesArray)]
-            
-                setFullName(uniqueNames)
-                setEmail(uniqueEmails)
-                setDietaryRestriction(dietaryRestrictsArray)
-                setResume(uniqueResumes)
-
-                } else {
-                console.log('No data found');
-                }
-            })
+              // Set the newData object as the state
+              setData(newData);
+            }
+          });
         }, []);
 
+        const handleCheckIn = (fullName) => {
+          // Toggle the checked-in status for the given fullName
+          setCheckedInStatus((prevStatus) => ({
+            ...prevStatus,
+            [fullName.toLowerCase()]: !prevStatus[fullName.toLowerCase()],
+          }));
+      
+          // Update the checkedInCount based on the new status
+          setCheckedInCount((prevCount) => {
+            return checkedInStatus[fullName.toLowerCase()] ? prevCount - 1 : prevCount + 1
+        }
+         );
+        };
+
         
-        const filteredResults = FullName.filter((fullName, index) => {
-          const email = Email[index];
+        const filteredResults = Object.keys(Data).filter((fullName) => {
+          const personData = Data[fullName];
+          const email = personData[0].email; // Get email from the person's data
         
-          const matchesQuery = fullName.toLowerCase().includes(Query.toLowerCase()) ||
+          const matchesQuery =
+            fullName.toLowerCase().includes(Query.toLowerCase()) ||
             (email && email.toLowerCase().includes(Query.toLowerCase()));
         
-          return matchesQuery 
+          return matchesQuery;
         });
        
 
@@ -93,28 +99,44 @@ function Search() {
     
           <h2>Name and Emails</h2>
           <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", gap: "20px"}}>
-          {filteredResults.map((results, index) => {
-              const dietaryRestrictionValue = dietaryRestriction[index];
-              if (!selectedDietaryRestriction || (dietaryRestrictionValue && dietaryRestrictionValue.includes(selectedDietaryRestriction))) {
-                return (
-                  <div key={index} style={{ border: "1px solid #ccc", padding: "10px" }}>
-                    <p>{results} - {Email.length} - {FullName.length}</p>
-                    <p>{dietaryRestrictionValue}</p>
-                    <p>{Email[index]}</p>
-                    {Resume[index] ? (
-                      <p>
-                        <a href={Resume[index]} target="_blank" rel="noopener noreferrer">
-                          {results} resume
-                        </a>
-                      </p>
-                    ) : null}
-                  </div>
-                );
-              } else {
-                return null; // Do not render if the dietary restriction does not match
-              }
-            })}
-          </div>
+          {filteredResults.map((fullName, index) => {
+            const personData = Data[fullName]; // Access the data associated with the FullName
+
+            const dietaryRestrictionValue = personData[0].dietaryRestriction; // Get dietaryRestriction from the person's data
+            const isCheckedIn = checkedInStatus[fullName.toLowerCase()] || false;
+
+            if (!selectedDietaryRestriction || (dietaryRestrictionValue && dietaryRestrictionValue.includes(selectedDietaryRestriction))) {
+              return (
+                <div key={index} style={{ border: "1px solid #ccc", padding: "10px" }}>
+                  <p>{fullName}</p>
+                  <p>{dietaryRestrictionValue}</p>
+                  <p>{personData[0].email}</p>
+                  {personData.map((data, dataIndex) => (
+                    <div key={dataIndex}>
+                      {data.resume ? (
+                        <p>
+                          <a href={data.resume} target="_blank" rel="noopener noreferrer">
+                            {fullName} resume
+                          </a>
+                        </p>
+                      ) : null}
+                      <button
+                        onClick={() => handleCheckIn(fullName)}
+                        style={{ backgroundColor: isCheckedIn ? "green" : "blue", color: "white" }}
+                      >
+                        {isCheckedIn ? "Checked In" : "Check In"}
+                      </button>
+                    </div>
+                  ))}
+                </div>
+              );
+            } else {
+              return null; // Do not render if the dietary restriction does not match
+            }
+          })}
+        </div>
+
+          <p>Total Checked In: {(checkedInCount)} - Percent Checked In: {((checkedInCount/Object.keys(Data).length) * 100)} % </p> 
         </div>
           );
 
@@ -124,4 +146,4 @@ function Search() {
 
 
 
-export default Search;
+export default Search
