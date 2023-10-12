c0e476abd2d0eb8042d34bcfabf6051e81787420
eduardocis <ecisnerosw@gmail.com>
2023-10-12 01:20:07 -0400

    Update firebase.js to use github secrets
    Made it so that firebaseConfig data is fetched from a .env.local file that only the tech committee will have downloaded. This makes it so that our keys, Ids, and Urls wont be public.

--- src/firebase.js
+++ src/firebase.js
@@ -7,22 +7,19 @@ import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAFHTfN6KuOwzDsLdWtmZeFAVpVcI-Npfw",
-    authDomain: "ideathon-registration-form.firebaseapp.com",
-    databaseURL: "https://ideathon-registration-form-default-rtdb.firebaseio.com",
-    projectId: "ideathon-registration-form",
-    storageBucket: "ideathon-registration-form.appspot.com",
-    messagingSenderId: "1094596378197",
-    appId: "1:1094596378197:web:81887879503a76e09c5edc",
-    measurementId: "G-JSB5Q51DBG"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const analytics = getAnalytics(app);
 export const storage = getStorage(app);
 export const database = getDatabase(app);
