0befa9e7b720e17fffd958598cf1070d49255183
Varun Reddy <varun04reddy@gmail.com>
2023-10-13 00:31:06 -0400

    finally

---
+++ src/Search.js
@@ -0,0 +1,184 @@
+import { onValue, ref, get, set } from 'firebase/database';
+import React, { useEffect, useState } from 'react';
+import { database } from './firebase';
+
+
+
+
+function Search() {
+    
+       const [Query, setQuery] = useState("")
+       const [dietaryRestriction, setDietaryRestriction] = useState([])
+       const [selectedDietaryRestriction, setSelectedDietaryRestriction] = useState("");
+       // varun stuff
+       
+       const [checkedInCount, setCheckedInCount] = useState(0);
+       
+
+
+
+       const [Data, setData] = useState({}); 
+
+        useEffect(() => {
+          onValue(ref(database), (snapshot) => {
+            const data = snapshot.val();
+            if (data) {
+              const newData = {}; 
+
+              for (const key in data) {
+                if (data.hasOwnProperty(key)) {
+                  const entry = data[key];
+                  
+                  const { firstName, lastName, email, dietaryRestriction, resume,checkedIn} = entry;
+                  const fullName = `${firstName} ${lastName}`;
+
+                  if (key != null) {
+                    if (!newData[key]) {
+                      newData[key] = [];
+                      newData[key].push({fullName,email, dietaryRestriction, resume,checkedIn});
+                    }
+                  }
+                }
+              }
+
+              // Set the newData object as the state
+              setData(newData);
+            }
+          });
+        }, []);
+
+       
+
+        const handleCheckIn = (key) => {
+          // Get a reference to the participant's check-in status in Firebase
+          const participantRef = ref(database, "/" + key);
+        
+          // Use the set function to update the check-in status
+          get(participantRef).then((snapshot) => {
+            const personData = snapshot.val() || false;
+            if(personData == false) {
+              return;
+            }
+            
+            
+            personData.checkedIn = !personData.checkedIn;
+            
+            set(participantRef, personData);
+          });
+        };
+
+        
+        const filteredResults = Object.keys(Data).filter((key) => {
+          const personData = Data[key];
+          const fullName = personData[0].fullName.toString();
+          const email = personData[0].email; // Get email from the person's data
+        
+          const matchesQuery =
+            fullName.toLowerCase().includes(Query.toLowerCase()) ||
+            (email && email.toLowerCase().includes(Query.toLowerCase()));
+        
+          return matchesQuery;
+        });
+       
+
+
+        return (
+          <div style={{ textAlign: 'center', background: '#232D4B', color: 'white', padding: '20px' }}>
+            <h1 style={{ fontSize: '60px' }}>Ideathon Admin Dashboard</h1>
+        
+            <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
+              {/* Search input */}
+              <input
+                type="text"
+                placeholder="Search by name or email"
+                value={Query}
+                onChange={(e) => setQuery(e.target.value)}
+                style={{
+                  width: '300px', // Adjust the width as needed
+                  height: '40px', // Adjust the height as needed
+                  fontSize: '16px' // Adjust the font size as needed
+                }}
+              />
+
+              <select
+                value={selectedDietaryRestriction}
+                onChange={(e) => setSelectedDietaryRestriction(e.target.value)}
+                style={{
+                  width: '300px', // Adjust the width as needed
+                  height: '46px', // Adjust the height as needed
+                  fontSize: '16px' // Adjust the font size as needed
+                }}
+              >
+                <option value="">All Dietary Restrictions</option>
+                <option value="vegetarian">Vegetarian</option>
+                <option value="gluten">Gluten-free</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
+
+        
+            <h2>Name and Emails</h2>
+            
+            <div style={{ display: "grid", gridTemplateColumns: "repeat(5, minmax(250px, 1fr))", gap: "20px" }}>
+              {filteredResults.map((key, index) => {
+                const personData = Data[key]; // Access the data associated with the key(hash)
+        
+                const dietaryRestrictionValue = personData[0].dietaryRestriction; // Get dietaryRestriction from the person's data
+                const fullName = personData[0].fullName.toString();
+                
+                const isCheckedIn = personData[0].checkedIn;
+        
+                if (!selectedDietaryRestriction || (dietaryRestrictionValue && dietaryRestrictionValue.includes(selectedDietaryRestriction))) {
+                  return (
+                    <div key={index} style={{ border: "1px solid #ccc", padding: "20px",transition: "background 0.1s", }}
+                    
+                    onMouseEnter={(e) => { e.target.style.transform = "scale(1.05)"; }} // Enlarge on hover
+                    onMouseLeave={(e) => { e.target.style.transform = "scale(1)"; }} // Return to the original size
+        
+                    
+                    >
+                      <p style={{ fontSize: '24px', fontWeight: 'bold' }}>{fullName}</p>
+
+                      <p>{dietaryRestrictionValue}</p>
+                      <p>{personData[0].email}</p>
+                      {personData.map((data, dataIndex) => (
+                        <div key={dataIndex}>
+                          {data.resume ? (
+                            <p>
+                            <a href={data.resume} target="_blank" rel="noopener noreferrer" style={{ color: 'orange' }}>
+                        {fullName} resume
+                            </a>
+                            </p>
+                          ) : null}
+                          <button
+                            onClick={() => handleCheckIn(key)}
+                            style={{ backgroundColor: isCheckedIn ? "green" : "blue", color: "white" }}
+                          >
+                            {isCheckedIn ? "Checked In" : "Check In"}
+                          </button>
+                        </div>
+                      ))}
+                    </div>
+                  );
+                } else {
+                  return null; // Do not render if the dietary restriction does not match
+                }
+              })}
+            </div>
+        
+            <p style={{ fontSize: '50px' }}>
+            {checkedInCount}|{((checkedInCount / Object.keys(Data).length) * 100)}%
+            </p>  
+          </div>
+        );
+        
+
+        
+
+        };
+
+    
+
+
+
+export default Search
--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Registration from './Registration';
 import reportWebVitals from './reportWebVitals';
+import Search from './Search';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Registration route />
   </React.StrictMode>
+  
+);
+
+root.render(
+  <React.StrictMode>
+    <Search route />
+  </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
