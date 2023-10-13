857a74dd79473cb43a3c3a14d1484d04a6f76c07
Varun Reddy <varun04reddy@gmail.com>
2023-10-13 01:59:02 -0400

    fixed checkedIn count to save the changes

--- src/Search.js
+++ src/Search.js
@@ -1,41 +1,69 @@
 import { onValue, ref, get, set } from 'firebase/database';
 import React, { useEffect, useState } from 'react';
 import { database } from './firebase';
-
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976D2', // Adjust primary color
+    },
+    secondary: {
+      main: '#F82249', // Adjust secondary color
+    },
+  },
+  typography: {
+    fontFamily: 'Arial, sans-serif', // Change font family
+    h1: {
+      fontSize: '32px', // Adjust heading font size
+      fontWeight: 'bold', // Bold headings
+    },
+    h2: {
+      fontSize: '24px',
+    },
+    // Add more typography styles as needed
+  },
+});
 
 
 
 function Search() {
+  
     
        const [Query, setQuery] = useState("")
        const [dietaryRestriction, setDietaryRestriction] = useState([])
        const [selectedDietaryRestriction, setSelectedDietaryRestriction] = useState("");
        // varun stuff
        
-       const [checkedInCount, setCheckedInCount] = useState(0);
+       
        
 
 
 
        const [Data, setData] = useState({}); 
+       const [checkedInCount, setCheckedInCount] = useState(0);
 
         useEffect(() => {
           onValue(ref(database), (snapshot) => {
             const data = snapshot.val();
             if (data) {
               const newData = {}; 
+              let newCheckedInCount = 0;
 
               for (const key in data) {
                 if (data.hasOwnProperty(key)) {
                   const entry = data[key];
                   
-                  const { firstName, lastName, email, dietaryRestriction, resume,checkedIn} = entry;
+                  const { firstName, lastName, email, dietaryRestriction, resume, checkedIn} = entry;
                   const fullName = `${firstName} ${lastName}`;
 
                   if (key != null) {
                     if (!newData[key]) {
                       newData[key] = [];
                       newData[key].push({fullName,email, dietaryRestriction, resume,checkedIn});
+
+                      if (checkedIn) {
+                        newCheckedInCount++;
+                      }
                     }
                   }
                 }
@@ -43,6 +71,7 @@ function Search() {
 
               // Set the newData object as the state
               setData(newData);
+              setCheckedInCount(newCheckedInCount);
             }
           });
         }, []);
@@ -64,6 +93,8 @@ function Search() {
             personData.checkedIn = !personData.checkedIn;
             
             set(participantRef, personData);
+
+           
           });
         };
 
@@ -79,49 +110,46 @@ function Search() {
         
           return matchesQuery;
         });
-       
-
 
         return (
-          <div style={{ textAlign: 'center', background: '#232D4B', color: 'white', padding: '20px' }}>
-            <h1 style={{ fontSize: '60px' }}>Ideathon Admin Dashboard</h1>
-        
-            <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
-              {/* Search input */}
-              <input
-                type="text"
-                placeholder="Search by name or email"
-                value={Query}
-                onChange={(e) => setQuery(e.target.value)}
-                style={{
-                  width: '300px', // Adjust the width as needed
-                  height: '40px', // Adjust the height as needed
-                  fontSize: '16px' // Adjust the font size as needed
-                }}
-              />
-
-              <select
-                value={selectedDietaryRestriction}
-                onChange={(e) => setSelectedDietaryRestriction(e.target.value)}
-                style={{
-                  width: '300px', // Adjust the width as needed
-                  height: '46px', // Adjust the height as needed
-                  fontSize: '16px' // Adjust the font size as needed
-                }}
-              >
-                <option value="">All Dietary Restrictions</option>
-                <option value="vegetarian">Vegetarian</option>
-                <option value="gluten">Gluten-free</option>
-                <option value="other">Other</option>
-              </select>
-            </div>
-
-        
-            <h2>Name and Emails</h2>
-            
-            <div style={{ display: "grid", gridTemplateColumns: "repeat(5, minmax(250px, 1fr))", gap: "20px" }}>
-              {filteredResults.map((key, index) => {
-                const personData = Data[key]; // Access the data associated with the key(hash)
+          <ThemeProvider theme={theme}>
+            <div style={{ textAlign: 'center', background: '#232D4B', color: 'white', padding: '20px' }}>
+              <h1 style={{ fontSize: '60px' }}>Ideathon Admin Dashboard</h1>
+              <p style={{ fontSize: '24px', textAlign: 'center' }}>
+                Checked In: {checkedInCount} | Percentage: {((checkedInCount / Object.keys(Data).length) * 100).toFixed(2)}%
+              </p>
+              <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
+                {/* Search input */}
+                <input
+                  type="text"
+                  placeholder="Search by name or email"
+                  value={Query}
+                  onChange={(e) => setQuery(e.target.value)}
+                  style={{
+                    width: '300px',
+                    height: '40px',
+                    fontSize: '16px',
+                  }}
+                />
+                <select
+                  value={selectedDietaryRestriction}
+                  onChange={(e) => setSelectedDietaryRestriction(e.target.value)}
+                  style={{
+                    width: '300px',
+                    height: '45px',
+                    fontSize: '16px',
+                  }}
+                >
+                  <option value="">All Dietary Restrictions</option>
+                  <option value="vegetarian">Vegetarian</option>
+                  <option value="gluten">Gluten-free</option>
+                  <option value="other">Other</option>
+                </select>
+              </div>
+              <h2>Name and Emails</h2>
+              <div style={{ display: "grid", gridTemplateColumns: "repeat(5, minmax(250px, 1fr))", gap: "20px" }}>
+                {filteredResults.map((key, index) => {
+                  const personData = Data[key]; // Access the data associated with the key(hash)
         
                 const dietaryRestrictionValue = personData[0].dietaryRestriction; // Get dietaryRestriction from the person's data
                 const fullName = personData[0].fullName.toString();
@@ -162,23 +190,17 @@ function Search() {
                   );
                 } else {
                   return null; // Do not render if the dietary restriction does not match
-                }
-              })}
+                  }
+                })}
+              </div>
             </div>
-        
-            <p style={{ fontSize: '50px' }}>
-            {checkedInCount}|{((checkedInCount / Object.keys(Data).length) * 100)}%
-            </p>  
-          </div>
+          </ThemeProvider>
         );
+      }
+      
+      export default Search;
         
+       
+
 
         
-
-        };
-
-    
-
-
-
-export default Search
