7851a8b395b8f7324763e419bb40a75ce1289a30
Diana Lin <lindiana1206@gmail.com>
2024-10-10 14:43:09 -0400

    feat: add dynamic expiry time for admin account

--- scripts/gen-admin.js
+++ scripts/gen-admin.js
@@ -1,5 +1,6 @@
 const jose = require("jose");
 const fs = require("fs");
+const readline = require("readline");
 
 async function createToken(payload, privKey) {
     const jwt = await new jose.SignJWT({
@@ -10,14 +11,23 @@ async function createToken(payload, privKey) {
     return jwt;
 }
 
-(async () => {
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question("Expiry Time (hours): ", async (hoursStr) => {
+    const hr = parseInt(hoursStr);
+
     const privKey = await jose.importPKCS8(fs.readFileSync("jwt.key", "utf8"));
 
     const token = await createToken({
         username: "admin",
         role: "admin",
-        exp: Math.floor(Date.now() / 1000) + 60 * 60,
+        exp: Math.floor(Date.now() / 1000) + 60 * 60 * hr,
     }, privKey);
 
     console.log(token);
-})();
+
+    rl.close();
+});
