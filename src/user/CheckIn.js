9af50be5182e4604d7b7bdd652437e741cae34c8
Diana Lin <52944826+dianalin2@users.noreply.github.com>
2025-10-13 21:50:46 -0400

    Fix typo in CheckIn.js

--- src/user/CheckIn.js
+++ src/user/CheckIn.js
@@ -5,7 +5,7 @@ import { Navigate } from "react-router-dom";
 
 function CheckIn() {
   
-  const { userCredential } = useAuth();u
+  const { userCredential } = useAuth();
   const uid = userCredential.user.uid;
 
   return (
